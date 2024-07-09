952b8029115e9dedc6f92def4fb97cefade4e8ba
Arslan <wowghost@bk.ru>
2024-07-09 16:51:30 +0500

    Add button in posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,12 +3,16 @@ import Post1 from '../../assets/img/post1.jpeg'
 import Post2 from '../../assets/img/post2.jpg'
 import Post3 from '../../assets/img/post3.jpeg'
 import Post4 from '../../assets/img/post4.avif'
+import Button from '../../shared/button/Button'
 
 
 const Posts = () => {
     return (
         <div className="posts">
-            <span className='posts__tittle'>Posts</span>
+            <div className="posts__header">
+                <span className='posts__tittle'>Posts</span>
+                <Button style={{ width: 100, height: 20, fontSize: 15 }}>Add post</Button>
+            </div>
             <hr className='posts__line' />
             <div className="posts__posts">
                 <img src={Post1} alt="" className='posts__post' />
